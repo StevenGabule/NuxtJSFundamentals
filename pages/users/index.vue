7e9676e68b7a3cf4502f1d7671045afa3cbb2dc3
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-01-26 12:29:33 +0800

    inital the setup for nuxtjs

---
+++ pages/users/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h1>Hello, users</h1>
+		<input type="text" v-model="id" placeholder="Enter a valid id" @keyup.13="loadUser">
+		<button @click="loadUser">Load User</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				id: ''
+			}
+		},
+		methods: {
+			loadUser() {
+				this.$router.push(`/users/${this.id}`);
+			}
+		}
+	}
+</script>
