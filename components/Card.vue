f2e2394d8bde0e690d5b16ffe0bdf9c985135707
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-01-26 17:05:03 +0800

    learn to create vue components with passing props

---
+++ components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+	  <div class="card-header">Post No. {{ post.id }}</div>
+	  <div class="card-body">
+	    <h5 class="card-title">{{ post.title}}</h5>
+	  </div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			post: Object
+		}
+	}
+</script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,33 +1,32 @@
 <template>
 	<div class="container">
+		<h2>Making API request - the Vue Way</h2>
 		<div class="row">
-			<div class="col">
-				<h2>Making API request - the Vue Way</h2>
-				<p v-for="post in posts" :key="post.id">{{ post.title }}</p>
-			</div>
+			<Card v-for="post in posts" :key="post.id" :post="post" class="ml-auto mr-auto" />
 		</div>
 	</div>
 </template>
 
 <script>
 	import axios from 'axios';
+	import Card from '@/components/Card';
 	export default {
+		components: {
+			Card
+		},
 		data() {
 			return {
-				posts: []
+				posts: [],
 			}
 		},
 
-		mounted() {
-			axios.get('https://jsonplaceholder.typicode.com/todos')
-				.then(response => {
-					console.log(response)
-					this.posts = response.data;
-				})
-				.catch(error => {
-					console.error(error)
-				})
-				return { posts: response.data}
+		async asyncData() {
+			let {data} = await axios.get('https://jsonplaceholder.typicode.com/todos');
+			return { posts: data}
+		},
+		
+		head: {
+			title: 'List of posts'
 		}
 	}
 </script>
